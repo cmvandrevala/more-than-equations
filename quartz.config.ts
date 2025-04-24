06cb47c2fb3da690ee075975333655b969404147
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2025-04-24 10:43:43 -0500

    Disable popover previews

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "More Than Equations",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: null,
     },
