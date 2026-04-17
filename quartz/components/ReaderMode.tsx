2b412153435e9937afc1dfde382a92ae203a4254
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2026-04-17 12:21:07 -0500

    Add img role to svg

--- quartz/components/ReaderMode.tsx
+++ quartz/components/ReaderMode.tsx
@@ -9,6 +9,7 @@ const ReaderMode: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps
   return (
     <button class={classNames(displayClass, "readermode")}>
       <svg
+        role="img"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
         version="1.1"
