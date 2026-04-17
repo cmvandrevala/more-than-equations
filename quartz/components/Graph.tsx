1362737b1e30097b659a1f4dbe46669eb5ae5f71
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2026-04-17 12:09:32 -0500

    Add img role to svg

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -70,6 +70,7 @@ export default ((opts?: Partial<GraphOptions>) => {
           <div class="graph-container" data-cfg={JSON.stringify(localGraph)}></div>
           <button class="global-graph-icon" aria-label="Global Graph">
             <svg
+              role="img"
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
               xmlnsXlink="http://www.w3.org/1999/xlink"
